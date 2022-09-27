38b87b9031670fdbace618fe5e7d267cb29d3626
Hugh Sun <wenjunsun99@gmail.com>
2022-09-26 20:47:53 -0700

    Finished Controller, Service, Mapper, thymeleaf for signup functionality. (At least for now)

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,15 +1,49 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
+    private UserService userService;
+
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public String getSignupPage(Model model) {
+        return "signup";
+    }
+
+    @PostMapping
+    public String signup(@ModelAttribute User user, Model model) {
+        String signupError = null;
+
+        if (!userService.isUserNameAvailable(user.getUserName())) {
+            signupError = "The username already exists. Please pick another username.";
+        }
+
+        if (signupError == null) {
+            int rowsAdded = userService.addUser(user);
+            if (rowsAdded < 0) {
+                signupError = "There was an error signing you up. Please try again.";
+            }
+        }
+
+        if (signupError == null) {
+            model.addAttribute("signupSuccess", true);
+        } else {
+            model.addAttribute("signupError", signupError);
+        }
+
         return "signup";
     }
 }
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,36 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+    private UserMapper userMapper;
+    private HashService hashService;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUserNameAvailable(String userName) {
+        return getUser(userName) == null;
+    }
+
+    public int addUser(User user) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insertUser(new User(null, user.getUserName(), encodedSalt, hashedPassword, user.getFirstName(), user.getLastName()));
+    }
+
+    public User getUser(String userName) {
+        return userMapper.getUser(userName);
+    }
+}
